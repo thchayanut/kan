e53ec868a5595b9dfcacd36716bf8b14c596ab83
Henry <henry_ball@hotmail.co.uk>
2025-08-04 11:29:17 +0100

    fix: only use cloud mailer client if creds exist

--- packages/email/src/cloudMailerClient.tsx
+++ packages/email/src/cloudMailerClient.tsx
@@ -1,7 +1,9 @@
 import Plunk from "@plunk/node";
 
 export const cloudMailerClient =
-  process.env.NEXT_PUBLIC_KAN_ENV === "cloud"
+  process.env.NEXT_PUBLIC_KAN_ENV === "cloud" &&
+  process.env.PLUNK_API_KEY &&
+  process.env.PLUNK_API_URL
     ? new Plunk(process.env.PLUNK_API_KEY ?? "", {
         baseUrl: process.env.PLUNK_API_URL,
       })
